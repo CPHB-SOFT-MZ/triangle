32615cfd3d02b02990c23858b94299e02da7ca13
Mikkel Ziemer <mzhjensen@gmail.com>
2018-02-14 13:01:51 +0100

    Added tests and refactored logic to match test cases

--- src/main/java/org/ziemer/test/triangle/Triangle.java
+++ src/main/java/org/ziemer/test/triangle/Triangle.java
@@ -1,21 +1,46 @@
 package org.ziemer.test.triangle;
 
 public class Triangle {
-    public static String determine(long sideA, long sideB, long sideC) {
+
+
+    public static State determine(long sideA, long sideB, long sideC) {
+
+
+        if (!isTriangle(sideA, sideB, sideC)) {
+            return State.NOT_A_TRIANGLE;
+        }
+
+        if (isEquiliteral(sideA, sideB, sideC)) {
+            return State.EQUILITERAL;
+        }
+
+        if (isIsosceles(sideA, sideB, sideC)) {
+            return State.ISOSCELES;
+        }
+
+        return State.SCALENE;
+    }
+
+    private static boolean isTriangle(long sideA, long sideB, long sideC) {
         if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
-            return "Not a triangle";
+            return false;
         }
 
-        if (sideA + sideB < sideC || sideA + sideC < sideB || sideC + sideB < sideA) {
-            return "Not a triangle";
-        }
+        return sideA + sideB > sideC && sideA + sideC > sideB && sideC + sideB > sideA;
+    }
 
-        if (sideA == sideB && sideB == sideC) {
-            return "Equilateral";
-        } else if (sideA == sideB || sideB == sideC || sideA == sideC) {
-            return "Isosceles";
-        } else {
-            return "Scalene";
-        }
+    private static boolean isEquiliteral(long sideA, long sideB, long sideC) {
+        return sideA == sideB && sideB == sideC;
+    }
+
+    private static boolean isIsosceles(long sideA, long sideB, long sideC) {
+        return sideA == sideB || sideB == sideC || sideA == sideC;
+    }
+
+    public enum State {
+        ISOSCELES,
+        EQUILITERAL,
+        SCALENE,
+        NOT_A_TRIANGLE
     }
 }
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -1,2 +1,66 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.ziemer.test.triangle.Triangle;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class TriangleTest {
+
+    @Test
+    public void testDetermineIsolescesTest() {
+        Triangle.State expected = Triangle.State.ISOSCELES;
+
+        // Test for failing
+        Triangle.State result = Triangle.determine(1, 1, 2);
+        Assert.assertNotEquals(expected, result);
+
+
+        result = Triangle.determine(2, 2, 3);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testDetermineequiliteralTest() {
+        Triangle.State expected = Triangle.State.EQUILITERAL;
+        Triangle.State result = Triangle.determine(2, 2, 2);
+        Assert.assertEquals(expected, result);
+
+        result = Triangle.determine(1, 1, 1);
+        Assert.assertEquals(expected, result);
+
+        result = Triangle.determine(999999, 999999, 999999);
+        Assert.assertEquals(expected, result);
+
+        for (int i = 0; i < 10; i++) {
+            int side = ThreadLocalRandom.current().nextInt(1, 999999 + 1);
+            result = Triangle.determine(side, side, side);
+            Assert.assertEquals(expected, result);
+        }
+    }
+
+    @Test
+    public void testDetermineScaleneTest() {
+        Triangle.State expected = Triangle.State.SCALENE;
+        // Test for failing
+        Triangle.State result = Triangle.determine(1, 2, 2);
+        Assert.assertNotEquals(expected, result);
+
+        result = Triangle.determine(2, 3, 4);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testDetermineNotTriangleTest() {
+        Triangle.State expected = Triangle.State.NOT_A_TRIANGLE;
+
+        Triangle.State result = Triangle.determine(1, 1, 2);
+        Assert.assertEquals(expected, result);
+
+        result = Triangle.determine(0, 2, 2);
+        Assert.assertEquals(expected, result);
+
+        result = Triangle.determine(-2, -2, -2);
+        Assert.assertEquals(expected, result);
+
+    }
 }
